ced241d6d6effb0fe16a6b7895e7b07c0ef00308
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2026-02-27 10:42:30 +0100

    Minor changes for ECAL base

--- Geometry/ECal_base_geo.cpp
+++ Geometry/ECal_base_geo.cpp
@@ -8,31 +8,25 @@ using dd4hep::Ref_t;
 using dd4hep::SensitiveDetector;
 
 
-class EcalBuild : public dd4hep::xml::tools::VolumeBuilder
+class NA64EcalBuild : public dd4hep::xml::tools::VolumeBuilder
 {
 public:
-    EcalBuild(dd4hep::Detector& theDetector, xml_elt_t xml_ent, dd4hep::SensitiveDetector sens_det);
-    virtual ~EcalBuild() {}
+    NA64EcalBuild(dd4hep::Detector& theDetector, xml_elt_t xml_ent, dd4hep::SensitiveDetector sens_det);
+    virtual ~NA64EcalBuild() {}
     dd4hep::Volume build_ecal();
-private:
-    double ecal_width;
-    double ecal_height;
-    double ecal_layer_depth;
-    int ecal_n_layers;
-
 };
 
-EcalBuild::EcalBuild(dd4hep::Detector& theDetector, xml_elt_t xml_ent, dd4hep::SensitiveDetector sens_det) :
+NA64EcalBuild::NA64EcalBuild(dd4hep::Detector& theDetector, xml_elt_t xml_ent, dd4hep::SensitiveDetector sens_det) :
     dd4hep::xml::tools::VolumeBuilder(theDetector, xml_ent, sens_det)
-{
-    ecal_width = dd4hep::_toDouble("ECALWidth");
-    ecal_height = dd4hep::_toDouble("ECALHeight");
-    ecal_layer_depth = dd4hep::_toDouble("ECALLayerDepth");
-    ecal_n_layers = dd4hep::_toInt("NLayersECAL");
-}
+{}
 
-dd4hep::Volume EcalBuild::build_ecal()
+dd4hep::Volume NA64EcalBuild::build_ecal()
 {
+    double ecal_width = dd4hep::_toDouble("ECALWidth");
+    double ecal_height = dd4hep::_toDouble("ECALHeight");
+    double ecal_layer_depth = dd4hep::_toDouble("ECALLayerDepth");
+    int ecal_n_layers = dd4hep::_toInt("NLayersECAL");
+
     dd4hep::Box ecal_box { ecal_width / 2, ecal_height / 2, ecal_layer_depth * ecal_n_layers / 2 };
     dd4hep::Volume ecal_vol { "ECalVol", ecal_box, description.material("Air") };
     return ecal_vol;
@@ -44,7 +38,7 @@ static Ref_t create_element(Detector& theDetector, xml_h xml_ent, SensitiveDetec
     std::string name = x_det.nameStr();
     sens_det.setType( "calorimeter" );    
 
-    EcalBuild builder { theDetector, x_det, sens_det };
+    NA64EcalBuild builder { theDetector, x_det, sens_det };
 
     dd4hep::Volume lvEcalVol = builder.build_ecal();
     dd4hep::PlacedVolume pv = builder.placeDetector(lvEcalVol);
@@ -53,4 +47,4 @@ static Ref_t create_element(Detector& theDetector, xml_h xml_ent, SensitiveDetec
     return builder.detector;
 }
 
-DECLARE_DETELEMENT(ECal_base, create_element)
+DECLARE_DETELEMENT(NA64ECal_base, create_element)
